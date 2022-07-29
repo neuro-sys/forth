5824d73954395f26989454b7da7b778982a11434
Firat Salgur <firatsalgur@gmail.com>
2022-07-29 20:28:59 +0300

    Add windows build script

--- compile-win32.bat
+++ compile-win32.bat
@@ -1,7 +1,6 @@
 cls
-C:\Users\neurosys\nasm-2.15.05\nasm -f win32 -w-zeroing -l forth-x86.lst -o forth-x86.obj forth-x86.asm
-link /subsystem:console /entry:start /SECTION:.text,RWE forth-x86.obj kernel32.lib user32.lib legacy_stdio_definitions.lib ucrt.lib
+C:\Users\neurosys\nasm-2.15.05\nasm -gcv8 -f win32 -w-zeroing -l forth-x86.lst -o forth-x86.obj forth-x86.asm
+link /debug:full /subsystem:console /entry:start /SECTION:.text,RWE /SECTION:.data,RWE /SECTION:.bss,RWE forth-x86.obj kernel32.lib user32.lib legacy_stdio_definitions.lib ucrt.lib
 forth-x86.exe
-
 rem done
 
